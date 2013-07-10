c6d30c7dfed4c6d49742078d0a5649e319f71250
Andrew Smith <andrewchristophersmith@gmail.com>
2013-07-10 09:47:39 -0700

    Working on mobile styles.

--- header.php
+++ header.php
@@ -57,4 +57,5 @@
 				'container' => 'nav',
 				'container_class' => 'primary-menu',
 				)); ?>
+        <a id="mobile-menu-link" href="#menu-primary-menu" onclick="jQuery('#menu-primary-menu').toggle();">Menu</a>
     </header>
--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
-var height_change_callback = function() {
-  if(($('.primary-menu').css('font-size') != '20px')) {
-    var bodyheight = jQuery(window).height();
-    jQuery("#feed-container .article-container").each(function() {
-  		jQuery(this).css("height", bodyheight-80);
-  	});
-  }
-}
-var width_change_callback = function() {
-	var bodywidth = jQuery(window).width();
-	jQuery("#main.horizontal .left article.post").each(function() {
-		jQuery(this).css("width", bodywidth-554);
-	});
-}
-jQuery(document).ready(height_change_callback);
-jQuery(document).ready(width_change_callback);
-// for the window resize
-jQuery(window).resize(height_change_callback);
-// for the horizontal post resize
-jQuery(window).resize(width_change_callback);
-
-// Hide all the submenus whenever we depart the area
-function hideSubMenus() {jQuery("ul.sub-menu").hide();}
-jQuery(".primary-menu").mouseleave(function() {hideSubMenus();});
-
-// An array of the top-level menus we want to use
-var primaryMenus = jQuery('#menu-primary-menu > li.menu-item');
-jQuery.each(primaryMenus, function(index, el) {
-	jQuery("#"+el.id).mouseenter(function() {
-		hideSubMenus();
-		jQuery("#"+el.id + " ul.sub-menu").show();
-	});
-});
-
 jQuery(function() {
- jQuery("#feed-container").width((jQuery(".article-container-wrapper").size() * 440));
+  // Things to do with desktop
+  if(($('.primary-menu').css('font-size') != '20px')) {
+   jQuery("#feed-container").width((jQuery(".article-container-wrapper").size() * 440));
+   
+   var height_change_callback = function() {
+     var bodyheight = jQuery(window).height();
+     jQuery("#feed-container .article-container").each(function() {
+   		jQuery(this).css("height", bodyheight-80);
+   	});
+   }
+   var width_change_callback = function() {
+   	var bodywidth = jQuery(window).width();
+   	jQuery("#main.horizontal .left article.post").each(function() {
+   		jQuery(this).css("width", bodywidth-554);
+   	});
+   }
+   jQuery(document).ready(height_change_callback);
+   jQuery(document).ready(width_change_callback);
+   // for the window resize
+   jQuery(window).resize(height_change_callback);
+   // for the horizontal post resize
+   jQuery(window).resize(width_change_callback);
+
+   // Hide all the submenus whenever we depart the area
+   function hideSubMenus() {jQuery("ul.sub-menu").hide();}
+   jQuery(".primary-menu").mouseleave(function() {hideSubMenus();});
+
+   // An array of the top-level menus we want to use
+   var primaryMenus = jQuery('#menu-primary-menu > li.menu-item');
+   jQuery.each(primaryMenus, function(index, el) {
+   	jQuery("#"+el.id).mouseenter(function() {
+   		hideSubMenus();
+   		jQuery("#"+el.id + " ul.sub-menu").show();
+   	});
+   });
+  } else {
+    // Do these with mobile
+    
+    // Scroll to where you can see the menu bar
+    jQuery(document).scrollTop($('div.article-container-wrapper').position().top);
+  }
 });
 
 // If the namenav.gif image exists, replaces the home button with it.
@@ -61,26 +67,3 @@ jQuery(function() {
     return (360 - this.width) / 2;
   });
 });
-
-// Mobile functions
-// jQuery(function(){
-//   if($('#feed-container').css('margin') == '0px') {
-//     var columnWidth = 340;
-//     var offset = 32;
-//     $('body').unbind();
-//     $('body').on('swipeleft', swipeleftHandler);
-//     $('body').on('swiperight', swiperightHandler);
-//     function swipeleftHandler(event) {
-//       var newPosition = parseInt($('#feed-container').css('left')) - columnWidth;
-//       if (($('.article-container-wrapper').size() * columnWidth) + newPosition > offset) {
-//         $('#feed-container').css('left', newPosition);
-//       }
-//     }
-//     function swiperightHandler(event) {
-//       var newPosition = parseInt($('#feed-container').css('left')) + columnWidth;
-//       if (newPosition <= offset) {
-//        $('#feed-container').css('left', newPosition); 
-//      }
-//     }
-//   }
-// });
