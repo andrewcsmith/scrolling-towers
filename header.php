ab47ec68f2f6204695e3efc57384742aac049d5d
Andrew Smith <andrewchristophersmith@gmail.com>
2013-08-02 11:43:20 -0700

    Made changes to mobile version

--- header.php
+++ header.php
@@ -57,5 +57,5 @@
 				'container' => 'nav',
 				'container_class' => 'primary-menu',
 				)); ?>
-        <div id="mobile-menu-link"><a href="#menu-primary-menu" onclick="jQuery('#menu-primary-menu').toggle();">Menu</a></div>
+        <div id="mobile-menu-link"><a href="#menu-primary-menu" onclick="jQuery('#menu-primary-menu').toggle();">MENU</a></div>
     </header>
